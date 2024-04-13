adc4cd88e123ce7717e617d6819fc5254ef6885c
Sher99 <shera.mamatbekov@gmail.com>
2024-04-13 22:09:43 +0600

    fix response country id from city list endpoint

--- app/Http/Controllers/Api/References/CityController.php
+++ app/Http/Controllers/Api/References/CityController.php
@@ -12,7 +12,7 @@ class CityController extends \App\Http\Controllers\Controller
      */
     public function list(): JsonResponse
     {
-        $cities = City::with('country:name')->get(['id', 'name', 'country_id'])->toArray();
+        $cities = City::with('country:id,name')->get(['id', 'name', 'country_id'])->toArray();
         $this->setResponse($cities);
 
         return $this->sendResponse();
