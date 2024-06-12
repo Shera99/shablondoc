dd25576b3e594aca4204c2b30046d79e3686f585
Sher99 <shera.mamatbekov@gmail.com>
2024-06-12 10:22:20 +0600

    fix for check logging http callback queries

--- app/Http/Controllers/Api/Payment/PaymentController.php
+++ app/Http/Controllers/Api/Payment/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Payment;
 
 use App\Http\Services\PaymentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class PaymentController extends \App\Http\Controllers\Controller
@@ -17,6 +18,8 @@ class PaymentController extends \App\Http\Controllers\Controller
 
     public function callBack(Request $request, string $secret): \Illuminate\Http\JsonResponse
     {
+        Log::channel('http')->info('Уведомление о платеже - ' . json_encode($request->all()));
+
         if ($secret !== config('app.payment_key'))
             return $this->sendErrorResponse('Incorrect access key!', Response::HTTP_UNAUTHORIZED);
 
