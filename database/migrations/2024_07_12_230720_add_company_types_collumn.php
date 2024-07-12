ce1bb5d6b4099a4aac7c524ddaaae2dde02b30a3
Sher99 <shera.mamatbekov@gmail.com>
2024-07-12 18:26:05 +0600

    company types added to company

---
+++ database/migrations/2024_07_12_230720_add_company_types_collumn.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->foreignId('company_type_id')->constrained('company_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropForeign(['company_type_id']);
+        });
+    }
+};
