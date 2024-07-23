45d4987b3082d8e3dbc7ae843fe45d0df0ca27b1
Sher99 <shera.mamatbekov@gmail.com>
2024-07-23 21:22:17 +0600

    fix create order for companies

--- routes/api.php
+++ routes/api.php
@@ -107,7 +107,7 @@ Route::get('/company-type', [CompanyController::class, 'companyType']);
 Route::get('/address', [CompanyAddressController::class, 'list']);
 Route::get('/template', [TemplateController::class, 'list']);
 Route::get('/currency', [CurrencyController::class, 'list']);
-Route::get('/translate/price', [OrderController::class, 'amount']);
+Route::get('/translate/price', [CurrencyController::class, 'amount']);
 Route::get('/translation-directions', [TranslationDirectionController::class, 'list']);
 
 Route::post('/order', [OrderController::class, 'create']);
