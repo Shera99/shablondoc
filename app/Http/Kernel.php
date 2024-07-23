8d3843f25ecbbca405e25f3d7ad273e50eaa785a
Sher99 <shera.mamatbekov@gmail.com>
2024-07-23 21:15:16 +0600

    fix create order for companies

---
+++ app/Http/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http;
+
+use Symfony\Component\HttpKernel\HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
